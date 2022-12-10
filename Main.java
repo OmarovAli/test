f0ecaa0a5b30e995c8f8d3ccac7b34fcacb37196
Ali Omarov <omarovali425@gmail.com>
2022-12-10 18:33:12 +0300

    java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] STALIN = {1, 2, 3, 4, 5,};
+        for (int i = 0; i < STALIN.length; i++) {
+            if (i < 5) {
+                System.out.println("Ahahahahahaha STALIN");
+            }
+        }
+    }
+}
